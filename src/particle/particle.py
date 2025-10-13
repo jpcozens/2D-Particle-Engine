7339933dfbb75f3e3057c4f29cb0610bf3abf182
jpcozens <jpcozens@gmail.com>
2025-10-13 17:25:55 +0100

    WIP: made typehinting more clean, increased code readability

--- src/particle/particle.py
+++ src/particle/particle.py
@@ -4,16 +4,19 @@ class Particle:
     MODE_CIRCLE = 1
 
     def __init__(self,
-                 pos: tuple[int,int],
+                 pos: tuple[int, int],
                  size: int,
                  mode: int,
-                 vel: tuple[float,float],
-    ):
+                 vel: tuple[float, float],
+                 ):
         pass
 
     def update(self): pass
 
     def set_mode(self, mode: int): pass
+
     def set_velocity(self, vel: tuple[float,float]): pass
+
     def set_accleration(self, acc: tuple[float,float]): pass
+
     def delete(self): pass
--- src/particle/particle_controller.py
+++ src/particle/particle_controller.py
@@ -9,14 +9,33 @@ class Particle_Controller:
                  particle_mode: int = Particle.MODE_RECT,
                  particle_size: int = 5,
                  particle_vel: tuple[float,float] = (5,5),
-    ):
-        self.particles = []
+                 ):
+
         self.rect = pygame.Rect(pos, size)
-        for _ in range(0,50): self.particles.append(Particle((random.randint(self.rect.left,self.rect.right),random.randint(self.rect.top,self.rect.bottom)),particle_size, particle_mode, particle_vel))
+        self.particles = []
+
+        # create 50 particles
+        for _ in range(0,50):
+            x = random.randint(self.rect.left,self.rect.right)
+            y = random.randint(self.rect.top,self.rect.bottom)
+
+            self.particles.append(
+                    Particle(
+                        (x,y),
+                        particle_size,
+                        particle_mode,
+                        particle_vel
+                    )
+            )
 
     def start(self): pass
+
     def clear(self): pass
+
     def set_particle_mode(self, particle_mode: int): pass
+
     def set_particle_velocity(self, particle_vel: tuple[float,float]): pass
+
     def set_particle_acceleration(self, particle_acc: tuple[float,float]): pass
+
     def delete_particle(self, particle: Particle): pass
