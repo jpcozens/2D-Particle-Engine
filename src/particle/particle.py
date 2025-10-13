8e7b1a71a940a35871b57e47b7437b4ed6d13d49
jpcozens <jpcozens@gmail.com>
2025-10-13 13:45:29 +0100

    WIP: added all method definitions with typehinting to particle.py

--- src/particle/particle.py
+++ src/particle/particle.py
@@ -1,4 +1,18 @@
 class Particle:
 
-    def __init__(self):
+    MODE_RECT = 0
+    MODE_CIRCLE = 1
+
+    def __init__(self,
+                 pos: tuple(int,int),
+                 size: int,
+                 mode: int,
+                 vel: tuple(float,float)):
         pass
+
+    def update(self): pass
+
+    def set_mode(mode: int): pass
+    def set_velocity(vel: tuple(float,float)): pass
+    def set_accleration(acc: tuple(float,float)): pass
+    def delete(self): pass
